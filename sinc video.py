8f01ff4ea6e1eee05596b28ecbb2a5e015ada6be
Simon Kuang <simontheflutist@berkeley.edu>
2021-03-21 13:55:59 -0700

    lol it's been so long that I couldn't understand my old code so I had to make it better

--- sinc video.py
+++ sinc video.py
@@ -32,29 +32,17 @@ def get_frame_windows(frames, width=3):
 
 
 def frame_weighted_avg(frames, weights):
-    # frames = np.asarray(list(frames), 'float32')
-    # weights = np.fromiter(weights, 'float32')
-    return np.tensordot(weights.astype('float32'), np.array(frames).astype('float32'), axes=1) / sum(weights)
-    # frame = next(frames)
-    # denom = next(weights)
-    # frame *= denom
-    #
-    # for (f, w) in zip(frames, weights):
-    #     frame += f * w
-    #     denom += w
-    #
-    # return frame / denom
+    return np.tensordot(
+        weights.astype('float32'),
+        np.array(frames).astype('float32'), axes=1
+    ) / sum(weights)
 
 
 def sinc_interp_frames(windows, factor=2):
     for window in windows:
         window_size = len(window)
         for i in range(factor):
-            # fractional offsets, for sinc
-            offsets = (np.arange(-window_size // 2,
-                                 -window_size // 2 + window_size)
-                       - i / factor)
-            weights = np.sinc(offsets)
+            weights = create_weights_old(i / factor, window_size)
 
             frame = frame_weighted_avg((window), (weights))
             # frame = resize_frame(frame[0, 0, :])
@@ -62,17 +50,43 @@ def sinc_interp_frames(windows, factor=2):
             yield frame
 
 
+def create_weights_old(pos, window_size):
+    """
+    Pos: position between 0 and 1 that this new frame is sampled from
+    """
+    # fractional offsets, for sinc.
+    # +0.5 to get in between frames and avoid "collapsing" on impulse
+    offsets = (np.arange(-window_size // 2,
+                         -window_size // 2 + window_size)
+               - pos) + 0.5
+    weights = np.sinc(offsets)
+    return weights
+
+
 def resize_frame(frame):
     frame_img = Image.fromarray(frame)
-    frame = np.asarray(frame_img.resize((3840, 2160),
-                                        resample=Image.LANCZOS))
+    # frame = np.asarray(frame_img.resize((3840, 2160),
+    #                                     resample=Image.LANCZOS))
     return frame
 
 
-def write_frames(frames, filename):
-    with skvideo.io.FFmpegWriter(filename) as writer:
-        for frame in frames:
+def write_frames(frames, filename, limit=600):
+    with create_writer(filename) as writer:
+        for i, frame in enumerate(frames):
+            if limit and i > limit:
+                break
             writer.writeFrame(frame)
+
+
+def create_writer(filename):
+    fps = 30
+    inputdict = {'-r': str(fps)}
+    outputdict = {'-r': str(fps)}
+    return skvideo.io.FFmpegWriter(
+        filename,
+        inputdict=inputdict,
+        outputdict=outputdict
+    )
 
 
 def main():
@@ -91,7 +105,7 @@ def main():
     frame_reader = skvideo.io.FFmpegReader(args.input)
     result_frames = sinc_interp_frames(
         get_frame_windows(frame_reader,
-                          width=256),
+                          width=12),
         factor=args.alpha)
     write_frames(tqdm(result_frames,
                       total=frame_reader.getShape()[0] * args.alpha),
